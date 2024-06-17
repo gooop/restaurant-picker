b473a16e3b51eb6a107118223dafa619546be8a1
Gavin Castaneda <gcastaneda456@gmail.com>
2024-06-16 18:38:55 -0600

    Initial Commit

---
+++ RestaurantPicker/RestaurantPickerApp.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantPickerApp.swift
+//  RestaurantPicker
+//
+//  Created by Gavin Castaneda on 6/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RestaurantPickerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
