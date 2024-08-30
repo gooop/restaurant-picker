ca68aaf61fa0baf32150f51f76c23dade1830d78
Gavin Castaneda <gcastaneda456@gmail.com>
2024-08-29 20:55:22 -0600

    add map and restaurant query button

--- RestaurantPicker/MainView.swift
+++ RestaurantPicker/MainView.swift
@@ -6,58 +6,34 @@
 //
 
 import SwiftUI
+import MapKit
 import SwiftData
+
+extension CLLocationCoordinate2D {
+    static let defaultLoc = CLLocationCoordinate2D(latitude: 39.7392, longitude: -104.9903)
+}
 
 struct MainView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State private var searchResults: [MKMapItem] = []
 
     var body: some View {
-        ZStack {
-            VStack {
-                Text("Guess the Flag")
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.black)
-                Spacer()
-                HStack {
-                    Button {
-                        //TODO: Do here
-                    } label: {
-                        Image("Burger")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200)
-                    }
-                    Button {
-                        //TODO: Do here
-                    } label: {
-                        Image("Pizza")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200)
-                    }
-                }
-                Spacer()
+        Map {
+            ForEach(searchResults, id: \.self) { result in
+                Marker(item: result)
             }
         }
+        .mapStyle(.standard(elevation: .realistic))
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                Spacer()
+                RestaurantSearchButton(searchResults: $searchResults)
+                    .padding(.top)
+                Spacer()
+            }
+            .background(.thinMaterial)
+        }
     }
-
-    
-
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(timestamp: Date())
-//            modelContext.insert(newItem)
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            for index in offsets {
-//                modelContext.delete(items[index])
-//            }
-//        }
-//    }
 }
 
 #Preview {
--- RestaurantPicker/RestaurantSearchButton.swift
+++ RestaurantPicker/RestaurantSearchButton.swift
@@ -5,4 +5,40 @@
 //  Created by Gavin Castaneda on 8/29/24.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+
+struct RestaurantSearchButton: View {
+
+    var body: some View {
+        HStack {
+            Button {
+                search(for: "restaurant")
+            } label: {
+                Label("Restaurants", systemImage: "fork.knife.circle.fill")
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .labelStyle(.iconOnly)
+    }
+
+    @Binding var searchResults: [MKMapItem]
+    
+    var mycenter = CLLocationCoordinate2D(latitude: 39.7392, longitude: -104.9903)
+
+    func search(for query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        request.resultTypes = .pointOfInterest
+        request.region = MKCoordinateRegion(
+            center: .defaultLoc,
+            span: MKCoordinateSpan(latitudeDelta: 0.0125, longitudeDelta: 0.0125))
+            
+        Task {
+            let search = MKLocalSearch(request: request)
+            let response = try? await search.start()
+            searchResults = response?.mapItems ?? []
+        }
+    }
+        
+}
